556e88dfc84ed03d1e775ec0193a6bbc74bf40fd
JemKeHU <staslovel1338@gmail.com>
2025-05-14 00:57:57 -0700

    cool calculator project

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let numberOne = 82;
-let numberTwo = 2;
-let operator = "+";
+let numberOne = null;
+let numberTwo = null;
+let operator = "";
+let calculated = false;
+let periodCount = 0;
 let input = document.querySelector("input");
 
 const sections = document.querySelectorAll(".sect");
@@ -9,53 +11,84 @@ for (const section of sections) {
     section.addEventListener("click", (e) => {
         let target = e.target;
 
-        if (target.id === "number") {
-            console.log(target.innerText);
+        if (target.tagName !== "BUTTON") return;
+        if (periodCount === 1) {
+            document.querySelector("#period").disabled = true;
         } else {
+            document.querySelector("#period").disabled = false;
+        }
+
+        if (target.id === "number") {
+            if (calculated) {
+                input.value = "";
+                numberOne = null;
+                numberTwo = null;
+                calculated = false;
+            }
+            input.value += target.innerText;
+        } else if (target.id === "backspace") {
+            if (input.value) {
+                if (input.value[input.value.length - 1] === ".") {
+                    periodCount = 0
+                    input.value = input.value.slice(0, -1);
+                } else {
+                    input.value = input.value.slice(0, -1);
+                }
+            }
+        } else if (target.id === "period") {
+            input.value += "."
+            periodCount++;
+        } else if (target.id === "add" || target.id === "subtract" || target.id === "divide" || target.id === "multiply") {
             console.log(target.id);
+            operator = target.id
+            numberOne = input.value;
+            input.value = "";
+            periodCount = 0;
+        } else if (target.id === "display") {
+            numberTwo = input.value;
+            numberOne = operate(operator, numberOne, numberTwo);
+            input.value = `${numberOne}`;
+            calculated = true;
+            periodCount = 0;
+        } else if (target.id === "delete") {
+            numberOne = null;
+            numberTwo = null;
+            input.value = "";
+            periodCount = 0;
         }
     });
 }
 
 const add = (numOne, numTwo) => {
-    return numOne + numTwo;
+    return Number(numOne) + Number(numTwo);
 };
 
 const subtract = (numOne, numTwo) => {
-    return numOne - numTwo;
+    return Number(numOne) - Number(numTwo);
 };
 
 const multiply = (numOne, numTwo) => {
-    return numOne * numTwo;
+    return Number(numOne) * Number(numTwo);
 };
 
 const divide = (numOne, numTwo) => {
-    if (!numTwo) {
+    if (!Number(numTwo)) {
         return "can't divide by zero";
     }
-    return numOne / numTwo;
+    return Number(numOne) / Number(numTwo);
 };
 
 const operate = (operation, numOne, numTwo) => {
     switch(operation) {
-        case "+":
-            input.value = add(numOne, numTwo);
-            break;
-        case "-":
-            input.value = subtract(numOne, numTwo);
-            break;
-        case "*":
-            input.value = multiply(numOne, numTwo);
-            break;
-        case "/":
-            input.value = divide(numOne, numTwo);
-            break;
+        case "add":
+            return add(numOne, numTwo);
+        case "subtract":
+            return subtract(numOne, numTwo);
+        case "multiply":
+            return multiply(numOne, numTwo);
+        case "divide":
+            return divide(numOne, numTwo);
         default:
-            input.value = "ERROR";
+            return "ERROR";
     }
 };
-
-console.log(operate(operator, numberOne, numberTwo));
-numberOne = 0;
-numberTwo = 0;
-operator = "";
